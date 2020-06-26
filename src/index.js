4e8123bf4e3de810a5b96f1fb82681be8b1fbef2
Pavel Martynov <martynovdev@gmail.com>
2020-06-26 09:07:37 +0300

    Add bold styling for current move & procedural creation of board cells

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ class Board extends React.Component {
 
   renderSquare(i) {
     return <Square 
+             key={i}
              status={this.props.status}
              value={this.props.squares[i]}
              onClick={() => {this.props.onClick(i)}}
@@ -24,24 +25,24 @@ class Board extends React.Component {
   }
 
   render() {
+    let rows = [];
+    for (let i = 0; i < 3; i++) {
+      let cells = [];
+      for (let j = 0; j < 3; j++) {
+        cells.push(this.renderSquare(j + i*3))
+      }
+      rows.push(
+      (<div className="board-row" key={i}>
+        {cells}
+      </div>)
+      )
+    }
+
+
     return (
       <div>
         <div className="status">{this.props.status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
@@ -117,8 +118,9 @@ class Game extends React.Component {
                           row #${row} 
                           col #${col}`: 
                          'move to start'; 
+      let isCurrent = this.state.stepNumber === index;                         
       return (
-        <li key={index}>
+        <li key={index} className={isCurrent ? 'current-move' : ''}>
           <button onClick={ () => {this.jumpTo(index)}}>
             {desc}
           </button>
